85a8a702f2d65c813d298885b9445cfa202365e7
efremzhenya <efremzhenya@yandex.ru>
2021-08-22 20:05:25 +0300

    Ex02

---
+++ Day 00/Ex02/Program.java
@@ -0,0 +1,55 @@
+package com.example.Program;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Integer number = 0, primeCount = 0;
+        while (number != 42) {
+            number = scanner.nextInt();
+            Integer sum = findSum(number);
+            if (sum < 2)
+            {
+                continue;
+            }
+            primeCount += isPrime(sum) ? 1 : 0;
+        }
+        System.out.print("Count of coffee-request – ");
+        System.out.print(primeCount);
+    }
+
+    private static Boolean isPrime(Integer number)
+    {
+        Integer root = findRoot(number);
+        Integer current = 2;
+        while (current <= root) {
+            if (number % current == 0 && number != current) {
+                return false;
+            }
+            current++;
+        }
+        return true;
+    }
+
+    private static Integer findSum(Integer number)
+    {
+        Integer sum = 0;
+        while (number > 0)
+        {
+            sum += number % 10;
+            number = number / 10;
+        }
+        return sum;
+    }
+
+    private static Integer findRoot(Integer number)
+    {
+        int i = 1;
+        int maxLimit = 46340;
+        while (i * i < number && i < maxLimit) {
+            i++;
+        }
+        return i;
+    }
+}
