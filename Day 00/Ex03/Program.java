e3f32cd0a7585945cee824070a027de0f65d53c4
efremzhenya <efremzhenya@yandex.ru>
2021-08-23 23:23:31 +0300

    add ex03 day00

---
+++ Day 00/Ex03/Program.java
@@ -0,0 +1,46 @@
+package com.company.Program;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String inputStr = sc.nextLine();
+        Integer week = 0;
+        long result = 0;
+        while (!inputStr.equals("42")) {
+            week++;
+            if (!inputStr.equals("Week " + week) || week > 18)
+                exitWithIllegalArgument();
+            Integer currentMinGrade = 9;
+            for (Integer j = 0; j < 5; j++) {
+                Integer inputGrade = sc.nextInt();
+                if (inputGrade < 1 || inputGrade > 9)
+                    exitWithIllegalArgument();
+                if (inputGrade < currentMinGrade)
+                    currentMinGrade = inputGrade;
+            }
+            sc.nextLine();
+            result = week == 1 ? currentMinGrade : result * 10 + currentMinGrade;
+            inputStr = sc.nextLine();
+        }
+        for (Integer k = 0; k < week; k++) {
+            System.out.print("Week ");
+            System.out.print(k + 1);
+            System.out.print(' ');
+            long del = 1;
+            for (Integer b = 1; b < week - k; b++)
+                del *= 10;
+            long grade = result / del;
+            result %= del;
+            for (long l = 0; l < grade; l++)
+                System.out.print('=');
+            System.out.print(">\n");
+        }
+    }
+
+    private static void exitWithIllegalArgument() {
+        System.err.println("IllegalArgument");
+        System.exit(-1);
+    }
+}
